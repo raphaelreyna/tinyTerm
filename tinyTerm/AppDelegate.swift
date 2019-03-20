7ad941cd62bd2973081ba05bd320c0f3b0c4be17
Raphael Reyna <raphaelreyna@protonmail.com>
2019-03-19 19:02:28 -0700

    Initial Commit

---
+++ tinyTerm/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  tinyTerm
+//
+//  Created by Raphael Reyna on 3/19/19.
+//  Copyright © 2019 Raphael Reyna. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
